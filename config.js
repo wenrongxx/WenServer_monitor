484002310c29316862f540edcbd973bc267ccee7
wenrongxx <121282497+wenrongxx@users.noreply.github.com>
2026-09-23 08:13:49 +0800

    CountDays 15; nav to official site / wiki / QQ

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'WenServer',
+  SiteName: 'WenServer 监控',
 
   // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm803822753-8e49da5f6019a8552b91c8a3',
     'm803823196-e37bf517e4e1d801cc7aaccd',
   ],
 
   // 日志天数
-  CountDays: 1,
+  CountDays: 15,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -19,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '← 返回官网',
+      url: 'https://ws.wenrongxx.top/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'Wiki',
+      url: 'https://wswiki.wenrongxx.top/'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'QQ 群',
+      url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=hUhfMx1teef%2FUTxXxdNV0ckd1TzfKTsl%2BWFtxwvfWHwREUKIrOCC76zvpbJw6RuB&busi_data=eyJncm91cENvZGUiOiIxMTI2NzU1MjM2IiwidG9rZW4iOiJrQXZqc3pVSXY3Z1VXdjNkTlFjTWV2MHVUd0pYSmJCRjlqMjcwYWJXWTFXZHpqd0dZUWZxbHZMS1FUcWswYW5vIiwidWluIjoiMzUzNjk3MzcwMSJ9&data=a372u44HNUhoORF6PJ8-bkdFYfwQ2sL6pcumTlYvozIGobXSfju4_hRJfLlPnZMhSXQeS_iVGuDnISM-edQl2w&svctype=4&tempid=h5_group_info'
     },
   ],
 };
